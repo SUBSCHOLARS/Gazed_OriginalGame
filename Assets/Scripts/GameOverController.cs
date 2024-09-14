28c6d8aa51e55117120d1cc9deba809f88320271
SUBSCHOLARS <kenkotan7722@gmail.com>
2024-09-14 13:28:40 +0900

    score appearence afte game over was prepared

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -5,12 +5,19 @@ using UnityEngine.UI;
 
 public class GameOverController : MonoBehaviour
 {
+    public Text SurvivedTimeScore;
+    public Text SurivivedTimeTextMinute;
     public Text SurvivedTimeText;
     // Start is called before the first frame update
     void Start()
     {
         float SurvivedTime = PlayerPrefs.GetFloat("SurvivedTime", 0f);
-        SurvivedTimeText.text = SurvivedTime.ToString("000");
+        int SurvivedTimeMinute = PlayerPrefs.GetInt("SurvivedTimeMinute", 0);
+        float SurvivedScore = PlayerPrefs.GetFloat("SurvivedScore", 0f);
+        SurvivedTimeText.text = SurvivedTime.ToString("00.0");
+        SurivivedTimeTextMinute.text = SurvivedTimeMinute.ToString("00");
+        SurvivedTimeScore.text = SurvivedScore.ToString("000");
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -53,7 +53,9 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.CompareTag("Cat"))
         {
             SceneManager.LoadScene("GameOver");
-            PlayerPrefs.SetFloat("SurvivedTime", SurvivalTimeController.ScorePotentialTimer);
+            PlayerPrefs.SetFloat("SurvivedScore", SurvivalTimeController.ScorePotentialTimer);
+            PlayerPrefs.SetFloat("SurvivedTime", SurvivalTimeController.timer);
+            PlayerPrefs.SetInt("SurvivedTimeMinute", SurvivalTimeMinuteDetector.MinuteDetector);
             /*Vector3 CollisionDirection = collision.contacts[0].normal;
             playerRB.AddForce(-CollisionDirection * Force, ForceMode.Impulse);*/
         }
